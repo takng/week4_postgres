b42fb0c82817cd13f3bc4d30f649eb8b2b82bbdf
TakNg <tak_ng@yahoo.com>
2017-07-18 23:17:42 +0000

    postgres

---
+++ migrations/20170718214722_migration_name.js
@@ -0,0 +1,19 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('beers', (table) => {
+      table.increments('id');
+      table.string('name', 255).notNullable();
+      table.decimal('abv').notNullable();
+      table.integer('brewery_id').unsigned().index();
+      table.timestamps();
+    }),
+    knex.schema.alterTable('tablename', (table) => {
+      table.string('website', 255);
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('beers');
+};
+
