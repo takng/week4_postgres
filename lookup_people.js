7de55da8dd68f5e6770b42f8e865c7026b8036e4
TakNg <tak_ng@yahoo.com>
2017-07-18 16:41:34 +0000

    postgres

---
+++ lookup_people.js
@@ -0,0 +1,30 @@
+let lastName = process.argv[2];
+console.log(lastName);
+
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * FROM famous_people WHERE last_name = $1 ", 
+               [lastName], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows[0].number); //output: 1
+    console.log(result.rows[0]); //output: 1
+    client.end();
+  });
+});
+
